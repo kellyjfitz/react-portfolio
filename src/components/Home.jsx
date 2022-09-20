25c1eaf02afa536899a361d965bbb993b094ac19
kellyjfitz <101250830+kellyjfitz@users.noreply.github.com>
2022-09-20 21:09:19 +1000

    Failed attempts to embed graph, used iframe for now

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,87 @@
 import React from "react";
 import Hero from "./Hero";
+import graph4 from "../images/graph4.png";
 
 function Home() {
   return (
-    <div className="home">
+    <div className="container">
       <Hero h1="Kelly Meleshko" />
+      <div className="section clearfix">
+        <img
+          src={graph4}
+          className="img-fluid  col-sm-6  float-sm-end sm-hide ms-3"
+          alt="A computer screen showing different
+          kinds of graphs"
+        />
+
+        <h3 className="mb-4">What's so good about data?</h3>
+
+        <p>
+          Print media has always been constrained by space and what can
+          physically be printed.
+        </p>
+        <p>
+          With the explosion of digial media, many organisations are seizing the
+          new storytelling opportunities that come with this medium.
+        </p>
+        <p>
+          One part of this is data visualisation. While print media might have
+          included a graph with a story, with digital media we can include a
+          slideshow of graphs, or a map animation showing change over time. Or
+          we can let the user interact with filters or clickable elements.{" "}
+        </p>
+        <p>
+          When I read a story with a bunch of numbers and stats, it doesn't
+          really sink in. But when someone visualises this same data, it becomes
+          much more meaningful and engaging.
+        </p>
+        <img
+          src={graph4}
+          className="img-fluid rounded  sm-show"
+          alt="A computer screen showing different kinds of graphs"
+        />
+      </div>
+
+      <div className="section row ms-0 me-0">
+        <h3 className="mb-4"> What's that got to do with coding?</h3>
+        <div className="col-md-6">
+          <p>It helps to understand how stuff works.</p>
+          <p>
+            If I have ideas about what would help build engagment with online
+            readers, it's really missing a lot of context without some
+            understanding about what is possible on a website and the effort
+            versus reward.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <p>
+            There are some very good (and free!) online tools such as{" "}
+            <a
+              href="https://www.datawrapper.de/"
+              target="_blank"
+              title="Datawrapper: Charts, maps, tables"
+              rel="noreferrer"
+            >
+              Datawrapper
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://flourish.studio/"
+              target="_blank"
+              title="Flourish Studio"
+              rel="noreferrer"
+            >
+              Flourish
+            </a>{" "}
+            that help you build data visualisations without any coding
+            knowledge.
+          </p>
+          <p>
+            But if you even have a tiny bit of knowledge you are able to really
+            grow what you can do.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,231 @@
 import React from "react";
 import Hero from "./Hero";
+import bluey2 from "../images/bluey2.png";
+import blueymobile from "../images/bluey-mobile.png";
+import weatherApp from "../images/weather-app2.png";
+import weatherMobile from "../images/weather-mobile.png";
 
 function Portfolio() {
+  const creditStyle = {
+    width: "100%!",
+    marginTop: "4px!important",
+    textAlign: "right!important",
+  };
+  const flourishImg =
+    "https://public.flourish.studio/resources/made_with_flourish.svg";
+  const imgStyle = {
+    width: "105px!important",
+    height: "16px!important",
+    border: "none!important",
+    margin: "0!important",
+  };
+
   return (
-    <div className="portfolio">
+    <div className="container">
       <Hero h1="Kelly Meleshko" />
+      <div className="section clearfix">
+        <a
+          href="https://sage-jalebi-b44e5a.netlify.app/"
+          target="_blank"
+          title="Weather app"
+          rel="noreferrer"
+        >
+          {" "}
+          <img
+            src={weatherApp}
+            alt="A screenshot of the weather app"
+            className="img-fluid  col-sm-6  float-sm-end ms-3 sm-hide"
+          />
+        </a>
+        <h3>
+          <a
+            href="https://sage-jalebi-b44e5a.netlify.app/"
+            target="_blank"
+            title="Weather app"
+            rel="noreferrer"
+          >
+            Weather app
+          </a>
+        </h3>
+        <p>This app was the final project for the SheCodes Plus workshop.</p>
+        <p>
+          It fetches data from the OpenWeather API based on a city search or the
+          user's current location. Although not required by the project, I set
+          the time and date to match the timezone of the location being searched
+          for rather than the user's timezone. For example; it is 9am in Sydney,
+          Australia but when I search for Kyiv the time in the weather app shows
+          me that it is 2am in Kyiv. I did this because I think the current
+          weather conditions are more meaningful if you have the current time.
+        </p>
+        <img
+          src={weatherMobile}
+          className="img-fluid   sm-show mt-1 mb-3"
+          alt="A screenshot of the weather app"
+        />
+        <p>
+          I also added a night theme to the project, which is conditional on the
+          local sunrise and sunset time. Again, using the Sydney versus Kyiv
+          example, at 9am in Sydney the site would have a light blue background
+          and black text. A search for Kyiv (where the local time was 2am) would
+          change the page to a navy blue background and white text.{" "}
+        </p>
+
+        <button className="btn btn-info">
+          {" "}
+          <a
+            href="https://sage-jalebi-b44e5a.netlify.app/"
+            target="_blank"
+            title="Weather app"
+            rel="noreferrer"
+          >
+            {" "}
+            Launch weather app
+          </a>
+        </button>
+      </div>
+
+      <div className="section clearfix">
+        <a
+          href="https://www.shecodes.io/workshops/shecodes-basics-fafed551-de54-4c46-8fff-483bff7a9b23/projects/796432"
+          target="_blank"
+          title="Bluey site"
+          rel="noreferrer"
+        >
+          {" "}
+          <img
+            src={bluey2}
+            alt="A screenshot of the Bluey website"
+            className="img-fluid  col-sm-6  float-sm-end ms-3 sm-hide"
+          />
+        </a>
+        <h3>
+          <a
+            href="https://www.shecodes.io/workshops/shecodes-basics-fafed551-de54-4c46-8fff-483bff7a9b23/projects/796432"
+            target="_blank"
+            title="Bluey site"
+            rel="noreferrer"
+          >
+            Bluey fan site
+          </a>
+        </h3>
+        <p>
+          If you have small children and you don't know about this Australian
+          cartoon - well, your life is about to change.{" "}
+        </p>
+        <p>
+          I built this site as the final project for the SheCodes Basics
+          workshop. It features basic HTML, CSS and Javascript.{" "}
+        </p>
+        <p>
+          When I built this I hadn't yet learnt about Bootstrap grids or Flexbox
+        </p>
+        <button className="btn btn-info">
+          {" "}
+          <a
+            href="https://www.shecodes.io/workshops/shecodes-basics-fafed551-de54-4c46-8fff-483bff7a9b23/projects/796432"
+            target="_blank"
+            title="Bluey site"
+            rel="noreferrer"
+          >
+            {" "}
+            Go to Bluey site
+          </a>
+        </button>
+        <a
+          href="https://www.shecodes.io/workshops/shecodes-basics-fafed551-de54-4c46-8fff-483bff7a9b23/projects/796432"
+          target="_blank"
+          title="Bluey site"
+          rel="noreferrer"
+        >
+          <img
+            src={blueymobile}
+            className="img-fluid   sm-show mt-4"
+            alt="A screenshot of the Bluey website"
+          />
+        </a>
+      </div>
+
+      <div className="visualisations">
+        <div className="section">
+          <div className="clearfix">
+            <h3>Data visualisations</h3>
+            <p>
+              I am always looking for opportunities to visualise data, whether
+              it be election results, Census data or even timelines.
+            </p>
+            <p>
+              I am interested in learning Python or R to build my expertise in
+              this area. At the moment I use{" "}
+              <a
+                href="https://www.datawrapper.de/"
+                target="_blank"
+                title="Datawrapper: Charts, maps, tables"
+                rel="noreferrer"
+              >
+                Datawrapper
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://flourish.studio/"
+                target="_blank"
+                title="Flourish Studio"
+                rel="noreferrer"
+              >
+                Flourish
+              </a>
+              . I have found my coding knowledge very useful in extending what's
+              possible with these tools.{" "}
+            </p>
+            <p>
+              I wrote a{" "}
+              <a
+                href="https://thewest.com.au/politics/federal-election/federal-election-2022-how-every-wa-electorate-voted-down-to-individual-polling-places-c-7068671"
+                target="_blank"
+                title="Election coverage"
+                rel="noreferrer"
+              >
+                story about election data
+              </a>{" "}
+              which used extensive graphs and maps.{" "}
+            </p>
+            <p>
+              These are some of the visualisations I have created in my current
+              role.
+            </p>
+
+            <div
+              className="flourish-embed flourish-cards"
+              data-src="visualisation/10554936"
+            >
+              <script src="https://public.flourish.studio/resources/embed.js"></script>
+            </div>
+
+            <iframe
+              src="https://flo.uri.sh/visualisation/10554936/embed"
+              title="Interactive or visual content"
+              className="flourish-embed-iframe"
+              style={{ width: "100%", height: "600px" }}
+              frameBorder="0"
+              scrolling="no"
+              sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+            ></iframe>
+            <div style={creditStyle}>
+              <a
+                className="flourish-credit"
+                href="https://public.flourish.studio/visualisation/10554936/?utm_source=embed&utm_campaign=visualisation/10554936"
+                target="_top"
+                style={{ textDecoration: "none!important" }}
+              >
+                <img
+                  alt="Made with Flourish"
+                  src={flourishImg}
+                  style={imgStyle}
+                />{" "}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
